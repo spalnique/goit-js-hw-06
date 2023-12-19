40d67cd01fa70b6d9993c4e85dbb4451d824743b
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-20 00:28:30 +0100

    done

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,46 @@
-function getShippingMessage(country, price, deliveryFee) {
-  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+class Storage {
+  #items;
+
+  constructor(productList) {
+    this.#items = productList;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+
+    // this.#items = this.#items.filter(x => x !== itemToRemove);
+
+    // this.#items = Array.from(this.#items, x => {
+    //   if (x !== itemToRemove) {
+    //     return x;
+    //   }
+    // });
+
+    // this.#items = this.#items.reduce((acc, x) => {
+    //   if (x !== itemToRemove) {
+    //     acc.push(x);
+    //   }
+    //   return acc;
+    // }, []);
+
+    // this.#items = this.#items.reduce(
+    //   (acc, x) => (x !== itemToRemove ? acc.concat([x]) : acc),
+    //   [],
+    // );
+  }
 }
 
-console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
