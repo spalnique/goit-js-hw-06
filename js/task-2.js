e527d676e91a1444f9ef08ff0f4d88fa31b2a350
Oleksii_S <lex.sydoruk@gmail.com>
2023-12-27 12:38:19 +0100

    fixed

--- js/task-2.js
+++ js/task-2.js
@@ -14,27 +14,8 @@ class Storage {
   }
 
   removeItem(itemToRemove) {
-    this.#items.splice(this.#items.indexOf(itemToRemove), 1);
-
-    // this.#items = this.#items.filter(x => x !== itemToRemove);
-
-    // this.#items = Array.from(this.#items, x => {
-    //   if (x !== itemToRemove) {
-    //     return x;
-    //   }
-    // });
-
-    // this.#items = this.#items.reduce((acc, x) => {
-    //   if (x !== itemToRemove) {
-    //     acc.push(x);
-    //   }
-    //   return acc;
-    // }, []);
-
-    // this.#items = this.#items.reduce(
-    //   (acc, x) => (x !== itemToRemove ? acc.concat([x]) : acc),
-    //   [],
-    // );
+    const index = this.#items.indexOf(itemToRemove);
+    index !== -1 ? this.#items.splice(index, 1) : this.#items;
   }
 }
 
